ed3671a29031127adf8d661b5d7d371678e693a3
Hrithik Joshi <hrithik.joshi@coxautoinc.com>
2024-11-07 14:28:48 +0530

    resolved dependency issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    //ksp
+    //kapt
     id("kotlin-kapt") apply true
 }
 
@@ -42,8 +42,6 @@ android {
 }
 
 dependencies {
-    val room_version = "2.6.1"
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
